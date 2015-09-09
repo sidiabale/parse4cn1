c3754d324089fcd6475c304af8ce4096f8dde971
Chidiebere Okwudire <sidiabale@gmail.com>
2015-09-09 19:51:05 +0200

    Test potential fix for ParseFileTest 3

--- test/CN1TestApp/test/com/parse4cn1/ParseFileTest.java
+++ test/CN1TestApp/test/com/parse4cn1/ParseFileTest.java
@@ -145,6 +145,7 @@ public class ParseFileTest extends BaseParseTest {
 
     private void uploadAndCheck(final String fileName) throws ParseException, IOException {
         System.err.println("Resource root path: " + getClass().getResource("/"));
+        System.err.println("Resource root path (classLoader): " + getClass().getClassLoader().getResource(""));
         assertNotNull(getClass().getResource("/" + fileName), "Test file missing");
         
         byte[] inputBytes = getBytes("/" + fileName);
