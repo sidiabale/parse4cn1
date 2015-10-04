fde94d5e292ed4d2911e7207dfa4bfbb54672955
Chidi Okwudire <sidiabale@gmail.com>
2015-10-04 20:55:32 +0200

    Extended logger class with options to retrieve log data

--- src/com/parse4cn1/util/Logger.java
+++ src/com/parse4cn1/util/Logger.java
@@ -17,19 +17,35 @@
 package com.parse4cn1.util;
 
 import com.codename1.io.Log;
+import com.codename1.io.Storage;
+import com.codename1.ui.Button;
+import com.codename1.ui.Command;
+import com.codename1.ui.Display;
+import com.codename1.ui.Form;
+import com.codename1.ui.TextArea;
+import com.codename1.ui.events.ActionEvent;
+import com.codename1.ui.events.ActionListener;
+import com.codename1.ui.layouts.BorderLayout;
+import com.parse4cn1.ParseException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 /**
  * Minimalist file logger. Logs to the default CN1 log file.
  * <p>
  * By default, debug logging is disabled and should <em>not</em> not be 
- * enabled in production code for performance and security reasons.
+ * enabled in production code for performance and security reasons 
+ * (sensitive information may be inadvertently logged).
  */
 public class Logger {
    
     private static Logger wrapper;
     private static final String PREFIX = "[parse4cn1] ";
+    // Use the CN1 default Log filename (see Log.createWriter() that other logging is also captured.
+    // Of course, this can break in whichh case only parse4cn1 logging will be present which is file.
+    private static final String LOG_FILENAME = "CN1Log__$"; 
     private final Log log;
-    
+
     public static Logger getInstance() {
         if (wrapper == null) {
             wrapper = new Logger();
@@ -42,27 +58,123 @@ public class Logger {
         setLogLevel(Log.INFO);
     }
 
+    /**
+     * Checks if debug logging is enabled.
+     * @return {@code true} if the debug logging is enabled; otherwise, returns {@code false}.
+     */
     public boolean isDebugEnabled() {
         return (Log.getLevel() == Log.DEBUG);
     }
     
+    /**
+     * Sets the desired log level.
+     * <p>
+     * {@code logLevel} and all higher log levels will be enabled after calling this method.
+     * @param logLevel The minimum log level.
+     */
     public final void setLogLevel(int logLevel) {
         Log.setLevel(logLevel);
     }
 
+    /**
+     * Logs a debug message.
+     * @param data The data to be logged.
+     */
     public void debug(String data) {
         log.p(PREFIX + data, Log.DEBUG);
     }
     
+    /**
+     * Logs an information message.
+     * @param data The data to be logged.
+     */
     public void info(String data) {
         log.p(PREFIX + data, Log.DEBUG);
     }
     
+    /**
+     * Logs a warning message.
+     * @param data The data to be logged.
+     */
     public void warn(String data) {
         log.p(PREFIX + data, Log.DEBUG);
     }
     
+    /**
+     * Logs an error message.
+     * @param data The data to be logged.
+     */
     public void error(String data) {
         log.p(PREFIX + data, Log.ERROR);
+    }
+    
+    /**
+     * Retrieves the entire log content as a single long string to be used by
+     * the application in any way it deems fit.
+     * 
+     * @return The log data if successfully retrieved or null.
+     * @throws com.parse4cn1.ParseException if anything goes wrong.
+     */
+    public String getLogContent() throws ParseException {
+        String text = null;
+        try {
+            Reader r = new InputStreamReader(Storage.getInstance().createInputStream(LOG_FILENAME));
+            char[] buffer = new char[1024];
+            int size = r.read(buffer);
+            while (size > -1) {
+                text += new String(buffer, 0, size);
+                size = r.read(buffer);
+            }
+            r.close();
+        } catch (Exception ex) {
+            throw new ParseException("Retrieving log file contents failed", ex);
+        }
+        return text;
+    }
+    
+    /**
+     * Creates and shows a form that contains the log data and an option to refresh it.
+     * <p>Back navigation from this form returns the app to the form that 
+     * was visible before this method was called.
+     * 
+     * @throws com.parse4cn1.ParseException if anything goes wrong.
+     * @see #getLogContent() 
+     */
+    public void showLog() throws ParseException {
+        try {
+            final String text = getLogContent();
+            final TextArea area = new TextArea(text, 5, 20);
+            area.setEditable(false);
+            final Form f = new Form("Log");
+            f.setScrollable(false);
+            final Form current = Display.getInstance().getCurrent();
+            Command back = new Command("Back") {
+                @Override
+                public void actionPerformed(ActionEvent ev) {
+                    current.show();
+                }
+            };
+            
+            final Button refreshButton = new Button("Refresh");
+            refreshButton.addActionListener(new ActionListener() {
+
+                public void actionPerformed(ActionEvent evt) {
+                    try {
+                        area.setText(getLogContent());
+                    } catch (ParseException ex) {
+                        error("Unable to show log contents. Error: " + ex);
+                   }
+                }
+            });
+            
+            f.addCommand(back);
+            f.setBackCommand(back);
+            f.setLayout(new BorderLayout());
+            f.addComponent(BorderLayout.CENTER, area);
+            f.addComponent(BorderLayout.NORTH, refreshButton);
+            f.show();
+        } catch (Exception ex) {
+            throw new ParseException("Unable to show log contents", ex);
+        }
     }
 }
