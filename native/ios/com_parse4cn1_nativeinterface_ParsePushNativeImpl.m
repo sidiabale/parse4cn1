d24182bcd2bf3e5014d769b29820577b4a077354
Chidi Okwudire <sidiabale@smash-ict.com>
2016-06-19 22:04:10 +0200

    Deleted native code for push notifications (forgot a file in earlier related commit). Will be restored when Open Source Parse Server-based push is implemented

--- native/ios/com_parse4cn1_nativeinterface_ParsePushNativeImpl.m
+++ native/ios/com_parse4cn1_nativeinterface_ParsePushNativeImpl.m
@@ -1,18 +1,12 @@
 #import "com_parse4cn1_nativeinterface_ParsePushNativeImpl.h"
-#import "Parse.h"
 
 @implementation com_parse4cn1_nativeinterface_ParsePushNativeImpl
 
 -(void)setBadge:(int)param{
-    PFInstallation *currentInstallation = [PFInstallation currentInstallation]; 
-    if (currentInstallation.badge != param) {
-        currentInstallation.badge = param;
-        [currentInstallation saveEventually];
-    }
 }
 
 -(BOOL)isSupported{
-    return YES;
+    return NO;
 }
 
 @end
