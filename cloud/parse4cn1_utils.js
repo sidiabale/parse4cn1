7abc853ea70dea5cfa0b830002f256467097b840
Chidi Okwudire <sidiabale@gmail.com>
2017-01-24 03:16:54 +0100

    Added file deletion utility to sample cloud code functions

--- cloud/parse4cn1_utils.js
+++ cloud/parse4cn1_utils.js
@@ -42,3 +42,37 @@ Parse.Cloud.define("sendPushViaRestApi", function(request, response) {
 	  });
   }
 });
+
+/** 
+ * Unconditionally deletes the file with the provided filename. This method should be used with care!!!
+ * <p><em>Note:</em> It was previously required by ParseInstallation in parse4cn1 but is no longer necessary thanks to this fix: https://github.com/ParsePlatform/parse-server/issues/1718
+ * 
+ * @filename: The name of the file to be deleted
+ * @param: server The Parse Server URL WITHOUT a trailing space.
+ */
+Parse.Cloud.define("deleteFile", function(request, response) {
+  
+  var filename = request.params.filename;
+  var server = request.params.server;
+  
+  if (!filename) {
+    response.error("Filename is not defined");
+  } else if (!server) {
+    response.error("Parse server URL (WITHOUT trailing backslash) is required")
+  } else {
+	  Parse.Cloud.httpRequest({
+		method: 'DELETE',
+		url: server + '/files/' + filename,
+		headers: {
+		  'X-Parse-Application-Id': Parse.applicationId,
+          'X-Parse-Master-Key': Parse.masterKey
+		},
+		success: function(httpResponse) {
+		  response.success(httpResponse.text);
+		},
+		error: function(httpResponse) {
+		  response.error("Request failed: " + httpResponse.text);
+		}
+	  });
+  }
+});
